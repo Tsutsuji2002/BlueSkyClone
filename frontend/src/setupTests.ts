edc30f5163d4460ef25908267e433cac233710dc
Nguyễn Thành Trung <nguyentrungtrung2002@gmail.com>
2026-02-14 11:51:23 +0700

    update dependences

--- frontend/src/setupTests.ts
+++ frontend/src/setupTests.ts
@@ -1,5 +1,15 @@
-// jest-dom adds custom jest matchers for asserting on DOM nodes.
-// allows you to do things like:
-// expect(element).toHaveTextContent(/react/i)
-// learn more: https://github.com/testing-library/jest-dom
 import '@testing-library/jest-dom';
+
+// Mock react-i18next
+jest.mock('react-i18next', () => ({
+    useTranslation: () => ({
+        t: (str: string) => str,
+        i18n: {
+            changeLanguage: () => new Promise(() => { }),
+        },
+    }),
+    initReactI18next: {
+        type: '3rdParty',
+        init: () => { },
+    },
+}));
