8f04f65862d0da0ee688c103feb1fac11c9fa15e
Nguyễn Thành Trung <nguyentrungtrung2002@gmail.com>
2026-03-18 19:45:18 +0700

    BlueSky Platform Features Upgrade

---
+++ backend/Migrations/20260318124338_AddThumbnailUrlToPostMedia.cs
@@ -0,0 +1,28 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace BSkyClone.Migrations
+{
+    /// <inheritdoc />
+    public partial class AddThumbnailUrlToPostMedia : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<string>(
+                name: "ThumbnailUrl",
+                table: "PostMedia",
+                type: "nvarchar(max)",
+                nullable: true);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "ThumbnailUrl",
+                table: "PostMedia");
+        }
+    }
+}
