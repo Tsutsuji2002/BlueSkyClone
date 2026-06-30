9087624a3b1fa2ffa1fa2dcc1c4b3e8aa9390e95
Nguyễn Thành Trung <nguyentrungtrung2002@gmail.com>
2026-07-01 00:54:20 +0700

    Main Loading UI update_v3

--- frontend/src/components/common/ButterflyLoading.tsx
+++ frontend/src/components/common/ButterflyLoading.tsx
@@ -7,21 +7,40 @@ interface ButterflyLoadingProps {
 }
 
 const ButterflyLoading: React.FC<ButterflyLoadingProps> = ({ size = 'md', className }) => {
-    const sizeClasses = {
-        sm: 'w-6 h-6',
-        md: 'w-12 h-12',
-        lg: 'w-24 h-24',
-        xl: 'w-32 h-32'
+    const sizeMap = {
+        sm: 24,
+        md: 48,
+        lg: 64,
+        xl: 96
     };
 
+    const svgSize = sizeMap[size];
+
     return (
-        <div className={cn("relative flex items-center justify-center", sizeClasses[size], className)}>
-            {/* Animated butterfly wings - CSS is in index.css */}
-            <div className="butterfly-container">
-                <div className="butterfly-wing butterfly-wing-left"></div>
-                <div className="butterfly-wing butterfly-wing-right"></div>
-                <div className="butterfly-body"></div>
-            </div>
+        <div className={cn("relative inline-block", className)} style={{ width: svgSize, height: (svgSize * 57) / 64 }}>
+            <svg
+                width={svgSize}
+                height={(svgSize * 57) / 64}
+                viewBox="0 0 64 57"
+                fill="none"
+                xmlns="http://www.w3.org/2000/svg"
+                style={{ position: 'absolute', top: 0, left: 0 }}
+            >
+                {/* Left wing - will flap */}
+                <g className="butterfly-left-wing" style={{ transformOrigin: '32px 28.5px' }}>
+                    <path
+                        d="M13.873 3.805C21.21 9.332 29.103 20.537 32 26.55v15.882c0-.338-.13.044-.41.867-1.512 4.456-7.418 21.847-20.923 7.944-7.111-7.32-3.819-14.64 9.125-16.85-7.405 1.264-15.73-.825-18.014-9.015C1.12 23.022 0 8.51 0 6.55 0-3.268 8.579-.182 13.873 3.805Z"
+                        fill="#0085ff"
+                    />
+                </g>
+                {/* Right wing - will flap */}
+                <g className="butterfly-right-wing" style={{ transformOrigin: '32px 28.5px' }}>
+                    <path
+                        d="M50.127 3.805C42.79 9.332 34.897 20.537 32 26.55v15.882c0-.338.13.044.41.867 1.512 4.456 7.418 21.847 20.923 7.944 7.111-7.32 3.819-14.64-9.125-16.85 7.405 1.264 15.73-.825 18.014-9.015C62.88 23.022 64 8.51 64 6.55c0-9.818-8.578-6.732-13.873-2.745Z"
+                        fill="#0085ff"
+                    />
+                </g>
+            </svg>
         </div>
     );
 };
